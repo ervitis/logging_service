9b81020ca1a6ae0a5f3b283a27bc9d9edbd9c398
ervitis <vitomarti@gmail.com>
2015-12-25 12:56:39 +0100

    feat:testing using circle

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+nosetests --verbosity=2
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,1 +1,37 @@
 # -*- coding: utf-8 -*-
+
+
+import unittest
+
+from logging_service import Logging
+
+
+class TestLogging(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_logging_cls(self):
+        self.assertIsNotNone(Logging)
+
+    def test_logging_set_services(self):
+        test_service = None
+
+        Logging.set_services([test_service])
+
+        self.assertIn(test_service, Logging._srvs)
+
+    def test_send_message_attribute_error_service_is_none(self):
+        test_service = None
+
+        Logging.set_services([test_service])
+
+        with self.assertRaises(AttributeError):
+            Logging.send('Hello')
+
+
+if __name__ == '__main__':
+    unittest.main()
