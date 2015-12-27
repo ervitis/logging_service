f502573df9c670a00e4746fecbe8a6353dae9042
ervitis <vitomarti@gmail.com>
2015-12-27 10:51:00 +0100

    build features branches

--- build.py
+++ build.py
@@ -23,26 +23,30 @@ def set_user_data_git(git):
 
 
 def main():
+    repo_path = os.path.abspath(os.path.dirname(__file__))
+    git = get_git(repo_path)
+
+    set_user_data_git(git)
+
     file_build = open_file('build_version')
     lines = file_build.readlines()
     build_version_old = lines[0]
+
+    new_tag_version = version + '-' + build_version_old
+    branch = 'branch-feature-' + new_tag_version
+    git('checkout', '-b', branch)
+
     build_version_new = str(int(build_version_old) + 1)
     lines = [line.replace(build_version_old, build_version_new) for line in lines]
     file_build.seek(0)
     file_build.writelines(lines)
     file_build.close()
 
-    repo_path = os.path.abspath(os.path.dirname(__file__))
-    git = get_git(repo_path)
-
-    set_user_data_git(git)
-
     git('add', '-u')
 
-    new_tag_version = version + '-' + build_version_old
     feature_message = 'feat: auto tag ' + new_tag_version
     git('commit', '-m', feature_message)
-    git('push', 'origin', 'master')
+    git('push', 'origin', '-u', branch)
 
     git('tag', new_tag_version)
     git('push', 'origin', '--tags')
